55f9496ad4c1162140a6ab70623d24b08fd47f57
Pankaj <pankaj@texlaculture.ai>
2024-11-20 14:34:36 +0530

    day 20 month 11 year 2024 time - 2:34pm

---
+++ src/FunctionsAssignmentsTotorial.java
@@ -0,0 +1,75 @@
+
+import java.util.Scanner;
+
+public class FunctionsAssignmentsTotorial {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+// Program 1
+        // System.out.print("Enter First Number: ");
+        // int a = sc.nextInt();
+        // System.out.print("Enter Second Number: ");
+        // int b = sc.nextInt();
+        // System.out.print("Enter Third Number: ");
+        // int c = sc.nextInt();
+        // maxAndMin(a, b, c);
+        // oddEvenProgram();
+        ageverify();
+    }
+
+// 1. Define two methods to print the maximum and the minimum number respectively among three numbers entered by the user.
+    // static void  maxAndMin(int a,int b,int c){
+    //     int max;
+        
+    //     if (a > b ) {
+    //         max = a;
+    //     }
+    //     else if (b > c) {
+    //         max = b;
+    //     }
+    //     else{
+    //         max = c;
+    //     }
+
+    //     int min = a;
+    //     if (b < min) {
+    //         min = b;
+    //     }
+    //     if (c<min) {
+    //         min = c;
+    //     }
+
+    //     System.out.format("Maximam Number form %d , %d and %d is = %d\n",a,b,c,max);
+    //     System.out.format("Minimun Number form %d , %d and %d is = %d\n",a,b,c,min);
+    // }
+
+// 2. Define a program to find out whether a given number is even or odd.
+    // static void oddEvenProgram(){
+    //     System.out.print("Enter a Number: ");
+    //     int num = sc.nextInt();
+    //     if (num%2 == 0) {
+    //         System.out.println("The given number "+num+" is Even Number.");
+    //     }else{
+    //         System.out.println("The given number "+num+" is Odd Number.");
+    //     }
+    // }
+
+// 3. A person is eligible to vote if his/her age is greater than or equal to 18. Define a method to find out if he/she is eligible to vote.
+    // static void ageverify(){
+    //     while(true){
+    //         System.out.print("Enter your age: ");
+    //         byte age = sc.nextByte();
+    //         if (age == 0){
+    //             break;
+    //         }
+    //         if (age < 0){
+    //             System.out.println("Age can't be negative.");
+    //         }else if (age < 18) {
+    //             System.out.println("You are not eligible to vote");
+    //         }else {
+    //             System.out.println("You are welcome to vote");
+    //         }   
+    //     }
+    // }
+
+// 4. Write a program to print the sum of two numbers entered by user by defining your own method.
+}

f12745f370172a20b30ddb0c24da9011dd282ef4
Pankaj <pankaj@texlaculture.ai>
2024-11-24 15:59:30 +0530

    Some new Commit i added Where i solve more leetcode questions

---
+++ src/arrrleetcode/GussGame.java
@@ -0,0 +1,11 @@
+package arrrleetcode;
+
+public class GussGame {
+    public static void main(String[] args) {
+     System.out.println(guessNumber(5));   
+    }
+
+    static int guessNumber(int n) {
+        
+    }
+}
---
+++ src/arrrleetcode/SmallerNumArr.java
@@ -0,0 +1,15 @@
+package arrrleetcode;
+
+import java.util.Arrays;
+
+public class SmallerNumArr {
+    public static void main(String[] args) {
+        int[] nums = {3,54,2,6};
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(nums)));
+    }
+
+    static int[] smallerNumbersThanCurrent(int[] nums) {
+
+        return new int[]{};
+    }
+}

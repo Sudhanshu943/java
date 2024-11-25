072fd3ded0a25ae49652b5c71a0dfe33f2d42270
Pankaj <pankaj@texlaculture.ai>
2024-11-25 22:31:48 +0530

    Some new questiond done

--- src/arrrleetcode/GussGame.java
+++ src/arrrleetcode/GussGame.java
@@ -1,11 +1,31 @@
-package arrrleetcode;
-
 public class GussGame {
     public static void main(String[] args) {
      System.out.println(guessNumber(5));   
     }
 
     static int guessNumber(int n) {
-        
+        int start = 0;
+        int end = n;
+
+        while (start<=end){
+            int mid = start +(end - start)/2;
+
+            int g = guess(mid);
+
+            switch (g) {
+                case 1 -> start = mid +1;
+                case -1 -> end = mid - 1;
+                default -> {
+                    return mid;
+                }
+            }
+        }
+
+        return 1;        
+    }
+
+    static int guess(int n){
+        int random = (int) (Math.random() * n);
+        return random;
     }
 }
--- src/arrrleetcode/SmallerNumArr.java
+++ src/arrrleetcode/SmallerNumArr.java
@@ -9,7 +9,17 @@ public class SmallerNumArr {
     }
 
     static int[] smallerNumbersThanCurrent(int[] nums) {
-
-        return new int[]{};
+        int count;
+        int[] arr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            count = 0;
+            for (int j = 0; j < nums.length;j++) {
+            if (j!=i && nums[j]<nums[i]) {
+                count++;
+            }                
+        }
+        arr[i] = count;    
+        }
+        return arr;
     }
-}
+}   
